fdfb91e60ae0b07351fb156e50bc6dd89c2e3b61
PABLO VALENTIN <pablo.valentin@etec.sp.gov.br>
2023-04-27 15:58:58 -0300

    Iniciando o desenvolvimento da classe Data Service.

---
+++ App_Banco_Digital/App_Banco_Digital/Service/Data_Service.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using System.Threading.Tasks;
+
+namespace App_Banco_Digital.Service
+{
+
+    public abstract class Data_Service
+    {
+
+        private static readonly string servidor = "http://10.0.2.2:8000";
+
+        protected static async Task<string> GetDataFromService(string rota)
+        {
+
+            return null;
+
+        }
+
+    }
+
+}
---
+++ App_Banco_Digital/App_Banco_Digital/Service/Data_Service_Correntista.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace App_Banco_Digital.Service
+{
+
+    public class Data_Service_Correntista
+    {
+
+
+
+    }
+
+}
