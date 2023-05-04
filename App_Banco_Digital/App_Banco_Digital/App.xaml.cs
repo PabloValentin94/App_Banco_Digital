d3b50b053398b142c78b383964a10a1ab4925c1b
PABLO VALENTIN <pablo.valentin@etec.sp.gov.br>
2023-05-04 15:11:27 -0300

    Criando a estrutura da página de cadastro de correntistas.

--- App_Banco_Digital/App_Banco_Digital/App.xaml.cs
+++ App_Banco_Digital/App_Banco_Digital/App.xaml.cs
@@ -4,25 +4,38 @@ using Xamarin.Forms.Xaml;
 
 namespace App_Banco_Digital
 {
+
     public partial class App : Application
     {
+
         public App()
         {
+
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new View.Correntista.Pages.Cadastro_Correntista();
+
         }
 
         protected override void OnStart()
         {
+
+
+
         }
 
         protected override void OnSleep()
         {
+
+
+
         }
 
         protected override void OnResume()
         {
+
+
+
         }
     }
 }
---
+++ App_Banco_Digital/App_Banco_Digital/View/Correntista/Pages/Cadastro_Correntista.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App_Banco_Digital.View.Correntista.Pages
+{
+
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Cadastro_Correntista : ContentPage
+    {
+
+        public Cadastro_Correntista()
+        {
+
+            InitializeComponent();
+
+        }
+
+    }
+
+}
